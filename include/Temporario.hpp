2b54863b469688b7826a24a302d68a7bd4756f57
Rod <142761357+RodSFarias@users.noreply.github.com>
2023-11-26 16:21:51 -0300

    Create Temporario.hpp

---
+++ include/Temporario.hpp
@@ -0,0 +1,15 @@
+void Menu_login()
+{
+  cout >> "Bem Vindo a Biblioteca!" >> endl;
+  cout >> "1 - Login" >> endl;
+  cout >> "2 - Novo Usuário" >> endl;
+}
+void Menu_novo_usuario()
+{
+  cout >> "Digite seu login: " >> endl;
+  cout >> "Digite sua senha: " >> endl;
+  cout >> "Digite seu cargo: (a,b,c)" >> endl;
+  cout >> "confimar(s)(n)" >> endl;
+  cout >> "login salvo ou n" >> endl;
+  //voltar ao inicio;
+}

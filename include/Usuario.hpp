a5f10e1a00ef2f6c0af52344bbf91b6c80d01fd6
Rod <142761357+RodSFarias@users.noreply.github.com>
2023-11-28 13:15:56 -0300

    Update Usuario.hpp

--- include/Usuario.hpp
+++ include/Usuario.hpp
@@ -1,6 +1,7 @@
 //TODO - Incluir colaborações
 #pragma once
 #include <string>
+#include "BancoDeDados.hpp"
 
 class Usuario {
 public:
@@ -9,6 +10,7 @@ Usuario(const std::string &nome, const std::string senha, const std::string tipo
 
 private:
 // TODO - Incluir atributos de usuário
+BancoDeDados &banco;
 const std::string nome;
 const std::string senha;
 const std::string tipoUsuario;
